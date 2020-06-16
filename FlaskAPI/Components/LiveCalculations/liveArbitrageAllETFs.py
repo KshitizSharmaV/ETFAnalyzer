000ae1f3d5a3af279465e6178e0370ae8318a187
piyush888 <piyush888@gmail.com>
2020-06-16 14:09:21 +0530

    Use csv files from CSV Files folder

--- FlaskAPI/Components/LiveCalculations/liveArbitrageAllETFs.py
+++ FlaskAPI/Components/LiveCalculations/liveArbitrageAllETFs.py
@@ -3,7 +3,7 @@ import pandas as pd
 class LiveArbitrageAllETFs(object):
 
 	def __init__(self):
-		allETFsList =pd.read_csv('./CalculateETFArbitrage/final365list.csv')
+		allETFsList =pd.read_csv('../CSVFiles/250M_WorkingETFs.csv')
 		ETFDBCategory = allETFsList['ETFdb.com Category']
 
 	def ETFDbCategoryList():
