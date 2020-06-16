07eb1a61ab13cdb7d858c5d54bec2b344bd719d0
piyush888 <piyush888@gmail.com>
2020-06-16 21:14:19 +0530

    Added file formatting for Log files

--- CommonServices/LogCreater.py
+++ CommonServices/LogCreater.py
@@ -4,18 +4,20 @@ import datetime
 
 class CreateLogger(object):
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def createLogFile(self,dirName=None, logFileName=None, loggerName=None):
-		path = os.path.join(os.getcwd(), dirName)
-		if not os.path.exists(path):
-			os.makedirs(path)
-		filename = path + datetime.datetime.now().strftime("%Y%m%d") + logFileName
-		handler = logging.FileHandler(filename)
-		logging.basicConfig(filename=filename, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filemode='a')
-		logger = logging.getLogger(loggerName)
-		logger.setLevel(logging.DEBUG)
-		logger.addHandler(handler)
-		return logger
+    def createLogFile(self,dirName=None, logFileName=None, loggerName=None):
+        path = os.path.join(os.getcwd(), dirName)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        filename = path + datetime.datetime.now().strftime("%Y%m%d") + logFileName
+        handler = logging.FileHandler(filename)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logging.basicConfig(filename=filename, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filemode='a')
+        logger = logging.getLogger(loggerName)
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+        return logger
 
