350396f3030a522464a5fc2ccfd26725efe7ba37
piyush888 <piyush888@gmail.com>
2020-06-24 22:10:15 +0530

    Bugfix: Handling saving errors for some ETFs.

--- CalculateETFArbitrage/Calculate_PNLdata_allETF.py
+++ CalculateETFArbitrage/Calculate_PNLdata_allETF.py
@@ -20,37 +20,42 @@ class CalculateAndSavePnLData():
         self.arbitragecollection = self.connforthis.ETF_db.ArbitrageCollection
     
     def retrievePNLForAllDays(self, magnitudeOfArbitrageToFilterOn=0):
-        date_list = self.returnres()
-        date_list.sort()
-        etflist = pd.read_csv('../CSVFiles/250M_WorkingETFs.csv').columns.to_list()
-        # final_res = []
-        for date in date_list:
-            all_etf_arb_cursor = self.arbitragecollection.find({'dateOfAnalysis': date})
-            PNLOverDates = {}
-            final_res = []
-            # Iter over the collection results
-            try:
-                for etf_arb in all_etf_arb_cursor:
-                    if etf_arb['ETFName'] in etflist:
-                        print(etf_arb['ETFName'])
-                        logger.debug(etf_arb['ETFName'])
-                        allData, pricedf, pnlstatementforday, scatterPlotData = AnalyzeArbitrageDataForETF(
-                            arbitrageDataFromMongo=etf_arb, magnitudeOfArbitrageToFilterOn=magnitudeOfArbitrageToFilterOn)
-                        PNLOverDates[str(etf_arb['ETFName'])] = pnlstatementforday
-                PNLOverDates = pd.DataFrame(PNLOverDates).T
-                # del PNLOverDates['Magnitue Of Arbitrage']
-                PNLOverDates.columns = ['Sell Return%','Buy Return%','Magnitue Of Arbitrage','# T_Buy','# R_Buy','# T_Sell','# R_Sell']
-                PNLOverDates['% R_Buy'] = round(PNLOverDates['# R_Buy'] / PNLOverDates['# T_Buy'], 2)
-                PNLOverDates['% R_Sell'] = round(PNLOverDates['# R_Sell'] / PNLOverDates['# T_Sell'], 2)
-                PNLOverDates['Date'] = date
-                PNLOverDates = PNLOverDates [['Date','Sell Return%','Buy Return%','# T_Buy','# R_Buy','% R_Buy','# T_Sell','# R_Sell','% R_Sell','Magnitue Of Arbitrage']]
-                final_res.extend(PNLOverDates.reset_index().rename(columns={'index': 'Symbol'}).to_dict('records'))
-            except Exception as e:
-                traceback.print_exc()
-                logger.exception(e)
-                # print("Exception in {}".format(etf_arb['ETFName']))
-                pass
-            self.Save_PnLData(final_res)
+        try:
+            date_list = self.returnres()
+            date_list.sort()
+            etflist = pd.read_csv('../CSVFiles/250M_WorkingETFs.csv').columns.to_list()
+            # final_res = []
+            for date in date_list:
+                all_etf_arb_cursor = self.arbitragecollection.find({'dateOfAnalysis': date})
+                PNLOverDates = {}
+                final_res = []
+                # Iter over the collection results
+                try:
+                    for etf_arb in all_etf_arb_cursor:
+                        if etf_arb['ETFName'] in etflist:
+                            print(etf_arb['ETFName'])
+                            logger.debug(etf_arb['ETFName'])
+                            allData, pricedf, pnlstatementforday, scatterPlotData = AnalyzeArbitrageDataForETF(
+                                arbitrageDataFromMongo=etf_arb, magnitudeOfArbitrageToFilterOn=magnitudeOfArbitrageToFilterOn)
+                            PNLOverDates[str(etf_arb['ETFName'])] = pnlstatementforday
+                    PNLOverDates = pd.DataFrame(PNLOverDates).T
+                    # del PNLOverDates['Magnitue Of Arbitrage']
+                    PNLOverDates.columns = ['Sell Return%','Buy Return%','Magnitue Of Arbitrage','# T_Buy','# R_Buy','# T_Sell','# R_Sell']
+                    PNLOverDates['% R_Buy'] = round(PNLOverDates['# R_Buy'] / PNLOverDates['# T_Buy'], 2)
+                    PNLOverDates['% R_Sell'] = round(PNLOverDates['# R_Sell'] / PNLOverDates['# T_Sell'], 2)
+                    PNLOverDates['Date'] = date
+                    PNLOverDates = PNLOverDates [['Date','Sell Return%','Buy Return%','# T_Buy','# R_Buy','% R_Buy','# T_Sell','# R_Sell','% R_Sell','Magnitue Of Arbitrage']]
+                    final_res.extend(PNLOverDates.reset_index().rename(columns={'index': 'Symbol'}).to_dict('records'))
+                except Exception as e:
+                    traceback.print_exc()
+                    logger.exception(e)
+                    # print("Exception in {}".format(etf_arb['ETFName']))
+                    pass
+                self.Save_PnLData(final_res)
+        except  Exception as e1:
+            logger.exception(e1)
+            traceback.print_exc()
+            pass
         # return final_res
     
     def retrievePNLForAllETF_ForOneDay(self, magnitudeOfArbitrageToFilterOn=0):
