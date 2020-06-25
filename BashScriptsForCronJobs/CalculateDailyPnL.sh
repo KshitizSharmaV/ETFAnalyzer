beee48d72efc1eeb74ec4c583cc5156201398045
piyush888 <piyush888@gmail.com>
2020-06-25 22:18:33 +0530

    Bugfix: Added fix to track status of zombie process as well.

--- BashScriptsForCronJobs/CalculateDailyPnL.sh
+++ BashScriptsForCronJobs/CalculateDailyPnL.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 PID=$(ps aux | grep -i "[p]ython Caller.py"  | awk '{print $2}')
-while [ -e /proc/$PID ]; do
-  sleep .6
-done
+lsof -p $PID +r 1 &>/dev/null
 source /home/ubuntu/etfenv/bin/activate
 cd /home/ubuntu/ETFAnalyzer/CalculateETFArbitrage/ || exit
 python Calculate_PNLdata_allETF.py
