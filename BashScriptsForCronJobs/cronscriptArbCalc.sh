8303f1551c8fea8659c6353c87818a3a2b0e145b
piyush888 <piyush888@gmail.com>
2020-06-29 12:13:47 +0530

    Feature: New cronscriptArbCalc.sh -- adjustments for new timed execution.

--- BashScriptsForCronJobs/cronscriptArbCalc.sh
+++ BashScriptsForCronJobs/cronscriptArbCalc.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-PID=$(ps aux | grep -i "[p]ython ProcessCaller.py"  | awk '{print $2}')
-while [ -e /proc/$PID ]; do
-  sleep .6
-done
 source /home/ubuntu/etfenv/bin/activate
 cd /home/ubuntu/ETFAnalyzer/CalculateETFArbitrage/ || exit
 python Caller.py
