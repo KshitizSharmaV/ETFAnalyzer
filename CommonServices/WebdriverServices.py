8ef00d0cf372e520156c5731bfe1f77974fbe90a
Piyush <piyush888@gmail.com>
2020-10-08 14:09:36 +0530

    BugFix: ElementClickInterceptedException resolved.

--- CommonServices/WebdriverServices.py
+++ CommonServices/WebdriverServices.py
@@ -54,7 +54,7 @@ class masterclass:
         self.driver.get("https://etfdb.com/members/login/")
         # wait only until the presence of 'login-button' is detected
         try:
-            element = WebDriverWait(self.driver, 10).until(
+            login = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.ID, "login-button")))
         except TimeoutException:
             print("Timeout exception caused by EC in Login ETFdb module")
@@ -67,10 +67,14 @@ class masterclass:
             print("Exception in WebdriverServices : {}".format(e))
             self.driver.quit()
 
-        e = self.driver.find_element(By.ID, "user_login")
-        e.send_keys("ticketsoft")
-        e = self.driver.find_element(By.ID, "password")
-        e.send_keys("etfapp2020")
-        e = self.driver.find_element(By.ID, "login-button")
-        e.click()
+        user_name = self.driver.find_element(By.ID, "user_login")
+        user_name.send_keys("ticketsoft")
+        password = self.driver.find_element(By.ID, "password")
+        password.send_keys("etfapp2020")
+        login = self.driver.find_element(By.ID, "login-button")
+        if login.is_enabled():
+            login.click()
+        else:
+            login.click()
+            login.click()
         time.sleep(3)
