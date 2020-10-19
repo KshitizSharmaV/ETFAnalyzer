8be9a5737b84466734155ebe445d8fea3cf995be
Piyush <piyush888@gmail.com>
2020-10-19 22:41:03 +0530

    Alpaca Trading V1

---
+++ AlpacaTrading/trade_funcs.py
@@ -0,0 +1,49 @@
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api.common import URL
+from AlpacaTrading.config import *
+
+base_url = "https://paper-api.alpaca.markets"
+
+api = tradeapi.REST(key_id=API_KEY, secret_key=SECRET_KEY, base_url=URL(base_url))
+
+
+def get_account_info():
+    return api.get_account().__dict__
+
+
+def get_all_orders(status=None, limit=None, after=None, until=None, direction=None, nested=None):
+    return api.list_orders(status=status, limit=limit, after=after, until=until, direction=direction,
+                           nested=nested)
+
+
+def get_order_by_order_id(order_id):
+    return api.get_order(order_id=order_id).__dict__
+
+
+def make_order(symbol, qty, side, _type='market', time_in_force='day', limit_price=None, stop_price=None,
+               client_order_id=None, order_class=None, take_profit=None, stop_loss=None, trail_price=None,
+               trail_percent=None):
+    return api.submit_order(symbol=symbol, qty=qty, side=side, type=_type, time_in_force=time_in_force,
+                            limit_price=limit_price, stop_price=stop_price, client_order_id=client_order_id,
+                            order_class=order_class, take_profit=take_profit, stop_loss=stop_loss,
+                            trail_price=trail_price, trail_percent=trail_percent)
+
+
+def cancel_all_open_orders():
+    return api.cancel_all_orders()
+
+
+def cancel_order_by_id(order_id):
+    return api.cancel_order(order_id=order_id)
+
+
+def get_last_trade_for_symbol(symbol):
+    return api.get_last_trade(symbol)
+
+
+# print(make_order('AAPL', 100, 'buy', 'market', 'day'))
+# print("#####*****#####")
+# print(get_all_orders(status='filled'))
+# print(get_order_by_order_id(order_id='668d6b14-0fb0-4919-a5c9-fed41506ebb6'))
+# print(cancel_order_by_id(order_id='668d6b14-0fb0-4919-a5c9-fed41506ebb6'))
+# print(cancel_all_open_orders())
---
+++ AlpacaTrading/trade_strategy.py
@@ -0,0 +1,112 @@
+#
+import sys
+
+sys.path.append("..")
+#
+import time
+import schedule
+import datetime
+import pprint
+import pandas as pd
+from typing import TypedDict
+from AlpacaTrading.trade_funcs import *
+from MongoDB.PerMinDataOperations import PerMinDataOperations
+
+
+class Position(TypedDict):
+    position_type: str
+    position_size: int
+    position_open_time: datetime.datetime
+    position_open_price: float
+    position_open_arbitrage: float
+    position_open_abs_arbitrage: float
+    position_close_time: datetime.datetime
+    position_close_price: float
+    position_close_arbitrage: float
+    position_close_abs_arbitrage: float
+    position_status: str
+
+
+position_dict = Position(position_type="", position_size=0, position_open_time=datetime.datetime.now(),
+                         position_open_price=0, position_open_arbitrage=0, position_open_abs_arbitrage=0,
+                         position_close_time=datetime.datetime.now(), position_close_price=0,
+                         position_close_arbitrage=0, position_close_abs_arbitrage=0, position_status="")
+
+
+def get_position_status(_position_dict):
+    """Determine Position Status -- Open or Close"""
+    if _position_dict['position_status'] == 'Open':
+        return True
+    else:
+        return False
+
+
+def open_position(_position_dict, position_for='VO', position_type="", position_size=0,
+                  position_open_time=datetime.datetime.now(), position_open_price=0, position_open_arbitrage=0,
+                  position_open_abs_arbitrage=0):
+    """Open position"""
+    if not get_position_status(_position_dict):
+        _position_dict['position_type'] = position_type
+        _position_dict['position_size'] = position_size
+        _position_dict['position_open_time'] = position_open_time
+        _position_dict['position_open_price'] = position_open_price
+        _position_dict['position_open_arbitrage'] = position_open_arbitrage
+        _position_dict['position_open_abs_arbitrage'] = position_open_abs_arbitrage
+        _position_dict['position_status'] = 'open'
+        make_order(symbol=position_for, qty=_position_dict['position_size'], side='buy')
+    return _position_dict
+
+
+def close_position(_position_dict, position_for='VO', position_close_time=datetime.datetime.now(),
+                   position_close_price=0, position_close_arbitrage=0, position_close_abs_arbitrage=0):
+    """Close an open position"""
+    if get_position_status(_position_dict):
+        _position_dict['position_close_time'] = position_close_time
+        _position_dict['position_close_price'] = position_close_price
+        _position_dict['position_close_arbitrage'] = position_close_arbitrage
+        _position_dict['position_close_abs_arbitrage'] = position_close_abs_arbitrage
+        _position_dict['position_status'] = 'close'
+        make_order(symbol=position_for, qty=_position_dict['position_size'], side='sell')
+    return _position_dict
+
+
+def fetch_arbitrage(etf_name='VO') -> pd.DataFrame:
+    record_df = PerMinDataOperations().LiveFetchPerMinArbitrage(etfname=etf_name)
+    return record_df
+
+
+def strategy_for_position(position_for='VO'):
+    """Decide when to open/close a Long position"""
+    print("Current Position status : ")
+    pprint.pprint(position_dict)
+    arbitrage_rec = fetch_arbitrage(etf_name=position_for)
+    print(f"Arbitrage Record: \n{arbitrage_rec}")
+    abs_arbitrage = abs(arbitrage_rec['Arbitrage in $'][0]) - arbitrage_rec['ETF Trading Spread in $'][0]
+    if get_position_status(position_dict):
+        close_position(position_dict, position_for=position_for,
+                       position_close_time=datetime.datetime.now().replace(second=0, microsecond=0),
+                       position_close_price=arbitrage_rec['ETF Price'],
+                       position_close_arbitrage=arbitrage_rec['Arbitrage in $'],
+                       position_close_abs_arbitrage=abs_arbitrage)
+        return
+    if abs_arbitrage > 0.10:
+        open_position(position_dict, position_for=position_for, position_type="long", position_size=5,
+                      position_open_time=datetime.datetime.now().replace(second=0, microsecond=0),
+                      position_open_price=arbitrage_rec['ETF Price'],
+                      position_open_arbitrage=arbitrage_rec['Arbitrage in $'],
+                      position_open_abs_arbitrage=abs_arbitrage)
+    print("Positions after strategy execution:")
+    pprint.pprint(position_dict)
+    print("########################################################################################################")
+
+
+def caller():
+    """scheduled every minute, Close the existing position if any, feed new arb data to strategy"""
+    schedule.every().minute.at(":08").do(strategy_for_position, 'VO')
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    caller()
