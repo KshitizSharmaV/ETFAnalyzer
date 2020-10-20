7fddfafe0075100c1ccf2a938bef0d9f9e0da1be
Piyush <piyush888@gmail.com>
2020-10-20 23:07:30 +0530

    bugfix for closing position, adjusted for multiple etfs

--- AlpacaTrading/trade_strategy.py
+++ AlpacaTrading/trade_strategy.py
@@ -14,6 +14,8 @@ from MongoDB.PerMinDataOperations import PerMinDataOperations
 
 
 class Position(TypedDict):
+    position_status: str
+    position_for: str
     position_type: str
     position_size: int
     position_open_time: datetime.datetime
@@ -24,18 +26,19 @@ class Position(TypedDict):
     position_close_price: float
     position_close_arbitrage: float
     position_close_abs_arbitrage: float
-    position_status: str
 
 
-position_dict = Position(position_type="", position_size=0, position_open_time=datetime.datetime.now(),
-                         position_open_price=0, position_open_arbitrage=0, position_open_abs_arbitrage=0,
-                         position_close_time=datetime.datetime.now(), position_close_price=0,
-                         position_close_arbitrage=0, position_close_abs_arbitrage=0, position_status="")
+# position_dict = Position(position_for='', position_type="", position_size=0, position_open_time=datetime.datetime.now(),
+#                          position_open_price=0, position_open_arbitrage=0, position_open_abs_arbitrage=0,
+#                          position_close_time=datetime.datetime.now(), position_close_price=0,
+#                          position_close_arbitrage=0, position_close_abs_arbitrage=0, position_status="")
+
+position_dicts = {}
 
 
 def get_position_status(_position_dict):
     """Determine Position Status -- Open or Close"""
-    if _position_dict['position_status'] == 'Open':
+    if _position_dict['position_status'] == 'open':
         return True
     else:
         return False
@@ -78,38 +81,51 @@ def fetch_arbitrage(etf_name='VO') -> pd.DataFrame:
 def strategy_for_position(position_for='VO'):
     """Decide when to open/close a Long position"""
     print("Current Position status : ")
-    pprint.pprint(position_dict)
+    pprint.pprint(position_dicts[position_for], sort_dicts=False)
+
     arbitrage_rec = fetch_arbitrage(etf_name=position_for)
     print(f"Arbitrage Record: \n{arbitrage_rec}")
     arbitrage = arbitrage_rec['Arbitrage in $'][0]
     spread = arbitrage_rec['ETF Trading Spread in $'][0]
     abs_arbitrage = abs(arbitrage) - spread if (arbitrage < 0 and abs(arbitrage) > spread) else 0
     print(f"Abs_Arbitrage: {abs_arbitrage}")
-    if get_position_status(position_dict):
-        close_position(position_dict, position_for=position_for,
+    if get_position_status(position_dicts[position_for]):
+        close_position(position_dicts[position_for], position_for=position_for,
                        position_close_time=datetime.datetime.now().replace(second=0, microsecond=0),
                        position_close_price=arbitrage_rec['ETF Price'][0],
                        position_close_arbitrage=arbitrage_rec['Arbitrage in $'][0],
                        position_close_abs_arbitrage=abs_arbitrage)
+        print("#*#*#*#*#* POSITION CLOSED #*#*#*#*#*")
         return
+
     if abs_arbitrage > 0.05:
-        open_position(position_dict, position_for=position_for, position_type="long", position_size=5,
+        open_position(position_dicts[position_for], position_for=position_for, position_type="long", position_size=5,
                       position_open_time=datetime.datetime.now().replace(second=0, microsecond=0),
                       position_open_price=arbitrage_rec['ETF Price'][0],
                       position_open_arbitrage=arbitrage_rec['Arbitrage in $'][0],
                       position_open_abs_arbitrage=abs_arbitrage)
+        print("#*#*#*#*#* POSITION OPENED #*#*#*#*#*")
+
     print("Positions after strategy execution:")
-    pprint.pprint(position_dict)
+    pprint.pprint(position_dicts[position_for], sort_dicts=False)
     print("########################################################################################################")
 
 
-def caller():
+def caller(etf_list):
     """scheduled every minute, Close the existing position if any, feed new arb data to strategy"""
-    schedule.every().minute.at(":08").do(strategy_for_position, 'VO')
+    for etf in etf_list:
+        position_dicts.update({etf: Position(position_for=etf, position_type="", position_size=0,
+                                             position_open_time=datetime.datetime.now(),
+                                             position_open_price=0, position_open_arbitrage=0,
+                                             position_open_abs_arbitrage=0,
+                                             position_close_time=datetime.datetime.now(), position_close_price=0,
+                                             position_close_arbitrage=0, position_close_abs_arbitrage=0,
+                                             position_status="")})
+        schedule.every().minute.at(":08").do(strategy_for_position, etf)
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 
 if __name__ == '__main__':
-    caller()
+    caller(['VO', 'FTEC'])
